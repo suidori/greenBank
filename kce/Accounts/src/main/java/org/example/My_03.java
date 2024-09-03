12c01cf40a9e56b73d9aa1ebfa6e5aa89eda1e2c
K-chaeeon <searheh@gmail.com>
2024-09-03 17:25:08 +0900

    no message

--- kce/Accounts/src/main/java/org/example/My_03.java
+++ kce/Accounts/src/main/java/org/example/My_03.java
@@ -1,10 +1,12 @@
 package org.example;
 /*
+직원메뉴-4번
  [계좌 정보 조회] (최근 거래 내역 5개)(select)
  ->계좌번호 입력 + 비밀번호 입력(int)
  -존재하지 않는 계좌번호
  -비밀번호 틀림
 
+직원메뉴-6번
 [계좌 개설 및 수정] (insert+update+delete)
 (개설)
 1. 신규개설
@@ -16,11 +18,6 @@ u_idx,a_password
 수정할 a_number -> 1.추가 / 2.삭제 u_idx
 2.계좌 비밀번호 수정
 수정할 a_number -> 수정 a_password
-
-[본인 계좌 조회](select)
-1.전체조회
-->로그인 된 사용자의 u_idx에 맞는 계좌번호,잔액 출력해줌
-
  */
 
 public class My_03 {
--- kce/Accounts/src/main/java/org/example/UserMenu.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-/*
-[본인 계좌 조회](select)
-1.전체조회
-->로그인 된 사용자의 u_idx에 맞는 계좌번호,잔액 출력해줌
-
- */
-public class UserMenu {
-}
