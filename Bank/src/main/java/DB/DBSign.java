51e363274dec9e0df0f13c85cc4f6173fe11077c
suidori <leo7750@naver.com>
2024-09-04 12:47:07 +0900

    0940commit

--- Bank/src/main/java/DB/DBSign.java
+++ Bank/src/main/java/DB/DBSign.java
@@ -1,24 +1,21 @@
 package DB;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class DBSign {
 
+    private boolean connection;
     private int u_idx;
-
-    public int getU_idx() {
-        return u_idx;
-    }
 
     Connection conn = null;
     PreparedStatement pstmt = null;
-    ResultSet userInfo = null;
-    ResultSet userId = null;
+    ResultSet rs = null;
 
-    public loginState login(Scanner sc) {
+    public DBSign() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             conn = DriverManager.getConnection(
@@ -26,46 +23,38 @@ public class DBSign {
                     "root",
                     "1234"
             );
-
-            pstmt = conn.prepareStatement("SELECT * FROM users;");
-            userInfo = pstmt.executeQuery();
-            System.out.println("""
-                                        
-                    1. 로그인
-                    아이디와 비밀번호를 입력 해 주세요.
-                    """);
-            System.out.print("아이디: ");
-            String idInput = sc.next();
-            System.out.print("비밀번호: ");
-            String passwordInput = sc.next();
-
-            while (userInfo.next()) {
-                if (idInput.equals(userInfo.getString("u_id"))) {
-                    if (passwordInput.equals(userInfo.getString("u_password"))) {
-                        System.out.println("로그인 성공");
-                        u_idx = userInfo.getInt("u_idx");
-                        if (userInfo.getString("u_level").equals("clerk")) {
-                            System.out.println(userInfo.getString("u_name") + " 직원 계정으로 로그인했습니다.");
-                            conn.close();
-                            return loginState.CLERK;
-                        } else {
-                            System.out.println(userInfo.getString("u_name") + " 고객님, 반갑습니다.");
-                            conn.close();
-                            return loginState.CUSTOMER;
-                        }
-                    } else {
-                        System.out.println("비밀번호가 올바르지 않습니다.");
-                        conn.close();
-                        return loginState.FAILED;
-                    }
-                }
-            }
+            connection = true;
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("연결 실패");
+            connection = false;
         }
-        System.out.println("존재하지 않는 아이디입니다.");
+    }
+
+    public boolean isConnection() {
+        return connection;
+    }
+
+    public loginState login(Scanner sc) {
+        System.out.println("아이디: ");
+        String inputId = sc.nextLine();
+        System.out.println("비밀번호: ");
+        String passwordInput = sc.nextLine();
+
         try {
-            conn.close();
+            pstmt = conn.prepareStatement("SELECT * FROM users WHERE u_id = ? AND u_password = ?");
+            pstmt.setString(1, inputId);
+            pstmt.setString(2, passwordInput);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                System.out.printf("권한: %s\n", (rs.getString("u_level").equals("clerk")) ? "직원" : "고객");
+                System.out.printf("%s님, 반갑습니다.");
+                return (rs.getString("u_level").equals("clerk")) ? loginState.CLERK : loginState.CUSTOMER;
+            } else {
+                System.out.println("아이디 또는 비밀번호가 일치하지 않습니다.");
+                return loginState.FAILED;
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -73,142 +62,55 @@ public class DBSign {
     }
 
     public boolean register(Scanner sc) {
+        System.out.println("회원 가입을 진행합니다.");
+
+        boolean repeat = true;
+        String inputId;
+        String inputPassword;
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(
-                    "jdbc:mysql://192.168.0.53:8888/Bank",
-                    "root",
-                    "1234"
-            );
-
-            System.out.println("""
+            while (repeat) {
+                System.out.println("""
                                         
-                    2. 회원가입
-                    아이디는 영어 대소문자와 숫자만 사용할 수 있습니다.""");
-            sc.nextLine();
-            boolean idCheck = true;
-            String idInput;
-            while (true) {
-                System.out.println("아이디를 입력 해 주세요");
-                idInput = sc.nextLine();
-                String checkInput;
-
-                if (idInput.contains(" ")) {
-                    System.out.println("아이디에 공백은 들어갈 수 없습니다.");
-                    continue;
-                } else if (idInput.isEmpty()) {
-                    continue;
-                }
-
-                checkInput = idInput.replaceAll("[0-9]", "");
-                checkInput = checkInput.replaceAll("[a-z]", "");
-                checkInput = checkInput.replaceAll("[A-Z]", "");
-
-                if (!checkInput.isEmpty()) {
-                    System.out.println("아이디는 영어 대소문자와 숫자만 사용할 수 있습니다.");
-                    continue;
-                }
-
-                pstmt = conn.prepareStatement("SELECT u_id FROM users;");
-                userId = pstmt.executeQuery();
-                while (userId.next()) {
-                    if (idInput.equals(userId.getString("u_id"))) {
-                        System.out.println("중복된 아이디입니다.");
-                        idCheck = false;
-                        break;
-                    }
-                }
-
-                if (idCheck) {
-                    break;
-                }
+                        초기 화면으로 돌아가고 싶다면 '취소'를 입력 해 주세요.
+                        아이디는 영어 대소문자, 숫자만 입력 가능합니다.
+                        아이디:
+                        """);
+                inputId = sc.nextLine();
+                cancle(inputId);
+                repeat = !isValidString(inputId);
             }
+            while (repeat) {
+                System.out.println("""
+                                        
+                        초기 화면으로 돌아가고 싶다면 '취소'를 입력 해 주세요.
+                        비밀번호는 영어 대소문자, 숫자만 입력 가능합니다.
+                        비밀번호:
+                        """);
+                inputPassword= sc.nextLine();
 
-            System.out.println("입력된 아이디는 [" + idInput + "] 입니다");
-
-            String passwordInput;
-
-
-            System.out.println("비밀번호는 영어 대소문자와 숫자만 입력할 수 있습니다.");
-            while (true) {
-                System.out.println("비밀번호를 입력 해 주세요.");
-                passwordInput = sc.nextLine();
-                String checkInput;
-
-                if (passwordInput.contains(" ")) {
-                    System.out.println("비밀번호에 공백은 들어갈 수 없습니다.");
-                    continue;
-                } else if (passwordInput.isEmpty()) {
-                    continue;
-                }
-
-                checkInput = passwordInput.replaceAll("[0-9]", "");
-                checkInput = checkInput.replaceAll("[a-z]", "");
-                checkInput = checkInput.replaceAll("[A-Z]", "");
-
-                if (!checkInput.isEmpty()) {
-                    System.out.println("비밀번호는 영어 대소문자와 숫자만 입력할 수 있습니다.");
-                } else {
-                    break;
-                }
+                cancle(inputPassword);
+                repeat = !isValidString(inputPassword);
             }
+        }catch (Exception e){
+            System.out.println("초기 화면으로 돌아갑니다.");
+        }
 
-            System.out.println("이름을 입력 해 주세요.");
-            String nameInput = sc.nextLine();
-            System.out.printf("당신의 이름은 %s 입니다.\n", nameInput);
+    }
 
-            String phoneInput;
+    private static boolean isValidString(String input) {
+        String regex = "^[a-zA-Z0-9]+$";
 
-            while (true) {
-                System.out.println("전화번호를 입력 해 주세요.");
-                phoneInput = sc.nextLine();
-
-                String regex = "\\d{3}-\\d{4}-\\d{4}";
-                Pattern pattern = Pattern.compile(regex);
-                Matcher matcher = pattern.matcher(phoneInput);
-
-                if (matcher.matches()) {
-                    System.out.printf("당신의 전화번호는 %s 입니다.\n", phoneInput);
-                    break;
-                } else {
-                    if (phoneInput.length() == 11) {
-                        phoneInput = phoneInput.replaceFirst("(\\d{3})(\\d{4})(\\d{4})", "$1-$2-$3");
-                        System.out.printf("당신의 전화번호는 %s 입니다.\n\n", phoneInput);
-                        break;
-                    } else {
-                        System.out.println("올바른 전화번호를 입력 해 주세요.");
-                    }
-                }
-            }
-
-            pstmt = conn.prepareStatement("INSERT INTO users (u_id, u_password, u_name, u_phone) VALUES (?, ?, ?, ?);");
-            pstmt.setString(1, idInput);
-            pstmt.setString(2, passwordInput);
-            pstmt.setString(3, nameInput);
-            pstmt.setString(4, phoneInput);
-            pstmt.executeUpdate();
-
-            pstmt = conn.prepareStatement("SELECT * FROM users WHERE u_id = ?;");
-            pstmt.setString(1, idInput);
-            userInfo = pstmt.executeQuery();
-            if (userInfo.next()) {
-                System.out.println("입력된 정보");
-                System.out.println("회원번호: " + userInfo.getInt("u_idx"));
-                System.out.println("아이디: " + userInfo.getString("u_id"));
-                System.out.println("비밀번호: " + userInfo.getString("u_password"));
-                System.out.println("이름: " + userInfo.getString("u_name"));
-                System.out.println("전화번호: " + userInfo.getString("u_phone"));
-            }
-            conn.close();
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            try {
-                conn.close();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
+        if (input == null) {
             return false;
+        }else if(!input.matches(regex)){
+            return false;
+        }
+
+    }
+
+    private static void cancle(String input){
+        if(input.equals("취소")){
+            throw new RuntimeException("회원 가입 취소");
         }
     }
 
--- Bank/src/main/java/Main.java
+++ Bank/src/main/java/Main.java
@@ -1,46 +1,56 @@
 import DB.*;
 import Menu.*;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        DBSign dbSign = new DBSign();
         System.out.println("초록은행에 오신 것을 환영합니다.");
-        while(true) {
+        boolean repeat = true;
+        while (repeat) {
             System.out.println("""
                             
                     메뉴를 선택 해 주세요.
                     1. 로그인
                     2. 회원가입
+                    3. 종료
                     """);
 
             Scanner sc = new Scanner(System.in);
             try {
                 switch (sc.nextInt()) {
                     case 1:
-                        loginState LS = dbSign.login(sc);
-                        if(LS == loginState.CLERK){
-                            ClerkMenu.c_main(dbSign.getU_idx(), sc);
-                        }else if (LS==loginState.CUSTOMER){
-//                            CustomerMenu.c_main(dbSign.getU_idx());
-                        }else{
-                            throw new RuntimeException();
+                        DBSign signIn = new DBSign();
+                        sc.nextLine();
+                        if(signIn.isConnection()) {
+                            loginState LS = signIn.login(sc);
+                            if (LS == loginState.CLERK) {
+                                ClerkMenu.c_main(signIn.getU_idx(), sc);
+                            } else if (LS == loginState.CUSTOMER) {
+//                            CustomerMenu.c_main(signIn.getU_idx());
+                            } else {
+                                repeat = false;
+                                throw new RuntimeException();
+                            }
+                        }else {
+                            repeat = false;
                         }
                         break;
                     case 2:
-                        if(dbSign.register(sc)){
-                            System.out.println("회원가입이 완료되었습니다.");
-                        }else{
-                            System.out.println("회원가입에 실패했습니다.");
-                        }
+                        DBSign signup = new DBSign();
+                        signup.register(sc);
                         throw new RuntimeException();
+                    case 3:
+                        System.out.println("프로그램을 종료합니다");
+                        repeat = false;
+                        break;
                     default:
                         throw new InputMismatchException();
                 }
                 break;
             } catch (Exception e) {
-                if(e instanceof InputMismatchException) {
+                if (e instanceof InputMismatchException) {
                     System.out.println("1이나 2를 입력 해 주세요");
                 }
             }
